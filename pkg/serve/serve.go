bf38e0d2e777828c8b4da6517fa06aaf7882d095
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-10-27 11:21:22 +0200

    serve: add redirect from replicate url

--- pkg/serve/serve.go
+++ pkg/serve/serve.go
@@ -2,6 +2,7 @@ package serve
 
 import (
 	"net/http"
+	"fmt"
 
 	"github.com/gorilla/mux"
 
@@ -16,6 +17,17 @@ func Start(port int, auth authn.Authenticator) error {
 	r.HandleFunc("/{user}/{model}:{version}", imageHandler)
 	r.HandleFunc("/{user}/{model}:{version}/{layer}", layerHandler)
 	r.HandleFunc("/{user}/{model}:{version}/{layer}/{file:.*}", fileHandler)
+	// yes, one slash, not sure why:
+	r.HandleFunc("/https:/replicate.com/{user}/{model}/versions/{version}", replicateComHandler)
 	http.Handle("/", r)
 	return http.ListenAndServe(":8080", nil)
+}
+
+func replicateComHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user := vars["user"]
+	model := vars["model"]
+	version := vars["version"]
+	newUrl := fmt.Sprintf("/%s/%s:%s", user, model, version)
+	http.Redirect(w, r, newUrl, http.StatusFound)
 }
